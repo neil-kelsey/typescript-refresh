7c180732e8bf6f9dd24ff4f840f5c8b5eb068ed0
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-31 10:14:08 +0100

    feat: Introducing interface version

--- 002-features/interfaces.ts
+++ 002-features/interfaces.ts
@@ -1,15 +1,25 @@
+interface Vehicle {
+    name: string;
+    year: number;
+    broken: boolean;
+}
+
 const oldCivic = {
     name: 'Civic',
     year: 2000,
     broken: true
 }
 
-// This is a long type annotation
 // This function is used to print out the above const
 // We expect an argument of vehicle
 // This argument has the types name, year and broken - we define their types
 // Then the output of the function is void as we're only going to log the output
-const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+
+// This is a long type annotation version 
+// const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+
+// This is the interface version
+const printVehicle = (vehicle: Vehicle): void => {
     console.log(`Name: ${vehicle.name}`);
     console.log(`Year: ${vehicle.year}`);
     console.log(`Broken: ${vehicle.broken}`);
